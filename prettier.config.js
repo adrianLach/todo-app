8423900dc326f2c46395d033ec696bb676eedf19
Adrian Lach <adrian.lach1999@gmail.com>
2024-08-20 12:31:05 +0200

    Files formatted

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none'
+}
 
-export default config;
+export default config
