243642baaefadf56ab810dcdac2987a9c83e9840
Adrian Lach <adrian.lach1999@gmail.com>
2024-08-20 12:35:15 +0200

    OAuth GitHub

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -49,8 +49,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_SECRET
     })
     /**
      * ...add more providers here.
